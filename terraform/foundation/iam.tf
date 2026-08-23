1925fd3fd761df702f4ae7fdaf8913e9eec5af38
raxj06 <rajvasoya06@gmail.com>
2026-08-23 08:22:26 +0530

    fix: add second GitHub OIDC thumbprint (1c58a3a8...fd23)

--- terraform/foundation/iam.tf
+++ terraform/foundation/iam.tf
@@ -4,7 +4,7 @@ data "aws_caller_identity" "current" {}
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a851ece13696bd07c68a74fc325336fd23"]
 }
 
 # IAM role assumed by GitHub Actions (main branch only)
